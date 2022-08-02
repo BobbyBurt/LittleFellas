9e204519928ea841e3826232218241cb9ad85184
BobbyBurt <aarontburt@gmail.com>
2022-08-01 22:34:23 -0400

    spawn sprites buttons

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -16,33 +16,6 @@ class Level extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// dustforce_SS
-		const dustforce_SS = this.add.image(1302, 0, "dustforce-SS-5");
-		dustforce_SS.setOrigin(0, 0);
-		dustforce_SS.flipX = true;
-
-		// dustforce_SS_5
-		const dustforce_SS_5 = this.add.image(-617, 0, "dustforce-SS-5");
-		dustforce_SS_5.setOrigin(0, 0);
-
-		// practice_dustman
-		const practice_dustman = this.add.image(1174, 464, "practice-dustman");
-
-		// dino_1
-		const dino_1 = this.add.image(1326, 99, "dino");
-		dino_1.setOrigin(0.5, 0);
-
-		// pants
-		const pants = this.add.sprite(813, 724, "dino");
-
-		// buttonTest
-		const buttonTest = this.add.rectangle(79, 962, 128, 128);
-		buttonTest.scaleX = 3.992183120085278;
-		buttonTest.scaleY = 1.8500935500799485;
-		buttonTest.setOrigin(0, 1);
-		buttonTest.isFilled = true;
-		buttonTest.fillColor = 3952106;
-
 		// buttonTest2
 		const buttonTest2 = this.add.rectangle(2472, 962, 128, 128);
 		buttonTest2.scaleX = 3.992183120085278;
@@ -57,53 +30,13 @@ class Level extends Phaser.Scene {
 		originPoint.scaleY = 0.1;
 		originPoint.isFilled = true;
 
-		// containerTest
-		const containerTest = this.add.container(1269, -162);
-
-		// rectangle_1
-		const rectangle_1 = this.add.rectangle(-2.849671494613318, 1.624822833873509, 128, 128);
-		rectangle_1.scaleX = 2.4564956310317054;
-		rectangle_1.scaleY = 1.7663375896109639;
-		rectangle_1.isFilled = true;
-		rectangle_1.fillColor = 13619151;
-		containerTest.add(rectangle_1);
-
-		// rectangle_2
-		const rectangle_2 = this.add.rectangle(4.389161020926281, 64.0585196911436, 128, 128);
-		rectangle_2.scaleX = 2.112951121688286;
-		rectangle_2.scaleY = 0.3517941570690122;
-		rectangle_2.isFilled = true;
-		containerTest.add(rectangle_2);
-
-		// ellipse_1
-		const ellipse_1 = this.add.ellipse(-78.61083897907372, -20.9414803088564, 128, 128);
-		ellipse_1.scaleX = 0.6985519488058136;
-		ellipse_1.scaleY = 0.7111560534682945;
-		ellipse_1.isFilled = true;
-		ellipse_1.fillColor = 4737096;
-		ellipse_1.isStroked = true;
-		ellipse_1.strokeColor = 16711422;
-		ellipse_1.lineWidth = 25;
-		containerTest.add(ellipse_1);
-
-		// ellipse
-		const ellipse = this.add.ellipse(67.38916102092628, -26.9414803088564, 128, 128);
-		ellipse.scaleX = 0.6985519488058136;
-		ellipse.scaleY = 0.7111560534682945;
-		ellipse.isFilled = true;
-		ellipse.fillColor = 4737096;
-		ellipse.isStroked = true;
-		ellipse.strokeColor = 16711422;
-		ellipse.lineWidth = 25;
-		containerTest.add(ellipse);
-
 		// MobileTextcontainer
 		const mobileTextcontainer = this.add.container(0, 0);
 
 		// rectangle_3
 		const rectangle_3 = this.add.rectangle(0, 0, 128, 128);
-		rectangle_3.scaleX = 1.2031249781092408;
-		rectangle_3.scaleY = 0.47656251661110416;
+		rectangle_3.scaleX = 1.402470438441591;
+		rectangle_3.scaleY = 0.6945006767374393;
 		rectangle_3.setOrigin(0, 0);
 		rectangle_3.isFilled = true;
 		rectangle_3.fillColor = 2894892;
@@ -116,29 +49,43 @@ class Level extends Phaser.Scene {
 		mobileText.setStyle({ "fontFamily": "Arial", "fontSize": "20px" });
 		mobileTextcontainer.add(mobileText);
 
+		// spriteCountText
+		const spriteCountText = this.add.text(20, 50, "", {});
+		spriteCountText.text = "sprites: 0";
+		spriteCountText.setStyle({ "fontFamily": "Arial", "fontSize": "20px" });
+		mobileTextcontainer.add(spriteCountText);
+
+		// MobileTextcontainer_1
+		const mobileTextcontainer_1 = this.add.container(2, 1021);
+
+		// rectangle
+		const rectangle = this.add.rectangle(0, -60.68319434475234, 128, 128);
+		rectangle.scaleX = 3.2396598487299233;
+		rectangle.scaleY = 0.47656251661110416;
+		rectangle.setOrigin(0, 0);
+		rectangle.isFilled = true;
+		rectangle.fillColor = 2894892;
+		rectangle.fillAlpha = 0.5;
+		mobileTextcontainer_1.add(rectangle);
+
+		// mobileText_1
+		const mobileText_1 = this.add.text(20, -40.68319434475234, "", {});
+		mobileText_1.text = "Use the right button to spawn more sprites";
+		mobileText_1.setStyle({ "fontFamily": "Arial", "fontSize": "20px" });
+		mobileTextcontainer_1.add(mobileText_1);
+
+		// testTween
+		const testTween = this.add.rectangle(407, 262, 128, 128);
+		testTween.isFilled = true;
+
 		// lists
-		const alignToCameraLeft = [buttonTest, mobileTextcontainer];
+		const alignToCameraLeft = [mobileTextcontainer, mobileTextcontainer_1];
 		const alignToCameraRight = [buttonTest2];
-		const alignToCameraTop = [mobileTextcontainer, dino_1];
-		const alignToCameraBottom = [buttonTest, buttonTest2];
-		const resizeScale = [dino_1, containerTest];
+		const alignToCameraTop = [mobileTextcontainer];
+		const alignToCameraBottom = [buttonTest2, mobileTextcontainer_1];
+		const resizeScale = [];
 		const alignToCameraMiddle = [];
-		const alignToCameraCenter = [dino_1];
-
-		// dino_1 (components)
-		new AlignOffsets(dino_1);
-
-		// pants (components)
-		new Animation(pants);
-
-		// buttonTest (components)
-		const buttonTestAlignOffsets = new AlignOffsets(buttonTest);
-		buttonTestAlignOffsets.x = 100;
-		buttonTestAlignOffsets.y = -100;
-		const buttonTestSimpleButton = new SimpleButton(buttonTest);
-		buttonTestSimpleButton.bindButton = "left";
-		const buttonTestDeviceDependentActivation = new DeviceDependentActivation(buttonTest);
-		buttonTestDeviceDependentActivation.ifMobile = true;
+		const alignToCameraCenter = [];
 
 		// buttonTest2 (components)
 		const buttonTest2AlignOffsets = new AlignOffsets(buttonTest2);
@@ -154,13 +101,15 @@ class Level extends Phaser.Scene {
 		mobileTextcontainerAlignOffsets.x = 20;
 		mobileTextcontainerAlignOffsets.y = 20;
 
-		this.dustforce_SS = dustforce_SS;
-		this.dustforce_SS_5 = dustforce_SS_5;
-		this.practice_dustman = practice_dustman;
-		this.pants = pants;
-		this.buttonTest = buttonTest;
+		// mobileTextcontainer_1 (components)
+		const mobileTextcontainer_1AlignOffsets = new AlignOffsets(mobileTextcontainer_1);
+		mobileTextcontainer_1AlignOffsets.x = 20;
+		mobileTextcontainer_1AlignOffsets.y = -20;
+
 		this.buttonTest2 = buttonTest2;
 		this.mobileText = mobileText;
+		this.spriteCountText = spriteCountText;
+		this.mobileText_1 = mobileText_1;
 		this.alignToCameraLeft = alignToCameraLeft;
 		this.alignToCameraRight = alignToCameraRight;
 		this.alignToCameraTop = alignToCameraTop;
@@ -172,33 +121,27 @@ class Level extends Phaser.Scene {
 		this.events.emit("scene-awake");
 	}
 
-	/** @type {Phaser.GameObjects.Image} */
-	dustforce_SS;
-	/** @type {Phaser.GameObjects.Image} */
-	dustforce_SS_5;
-	/** @type {Phaser.GameObjects.Image} */
-	practice_dustman;
-	/** @type {Phaser.GameObjects.Sprite} */
-	pants;
-	/** @type {Phaser.GameObjects.Rectangle} */
-	buttonTest;
 	/** @type {Phaser.GameObjects.Rectangle} */
 	buttonTest2;
 	/** @type {Phaser.GameObjects.Text} */
 	mobileText;
-	/** @type {Array<Phaser.GameObjects.Rectangle|Phaser.GameObjects.Container>} */
+	/** @type {Phaser.GameObjects.Text} */
+	spriteCountText;
+	/** @type {Phaser.GameObjects.Text} */
+	mobileText_1;
+	/** @type {Phaser.GameObjects.Container[]} */
 	alignToCameraLeft;
 	/** @type {Phaser.GameObjects.Rectangle[]} */
 	alignToCameraRight;
-	/** @type {Array<Phaser.GameObjects.Container|Phaser.GameObjects.Image>} */
+	/** @type {Phaser.GameObjects.Container[]} */
 	alignToCameraTop;
-	/** @type {Phaser.GameObjects.Rectangle[]} */
+	/** @type {Array<Phaser.GameObjects.Rectangle|Phaser.GameObjects.Container>} */
 	alignToCameraBottom;
-	/** @type {Array<Phaser.GameObjects.Image|Phaser.GameObjects.Container>} */
+	/** @type {Array<any>} */
 	resizeScale;
 	/** @type {Array<any>} */
 	alignToCameraMiddle;
-	/** @type {Phaser.GameObjects.Image[]} */
+	/** @type {Array<any>} */
 	alignToCameraCenter;
 
 	/* START-USER-CODE */
@@ -224,19 +167,25 @@ class Level extends Phaser.Scene {
 		this.controls.bindKey('left', keys.left_1);
 		this.controls.bindKey('right', keys.right_1);
 
-		this.controls.bindButton(SimpleButton.getComponent(this.buttonTest).bindButton, SimpleButton.getComponent(this.buttonTest));
+		// this.controls.bindButton(SimpleButton.getComponent(this.buttonTest).bindButton, SimpleButton.getComponent(this.buttonTest));
 		this.controls.bindButton(SimpleButton.getComponent(this.buttonTest2).bindButton, SimpleButton.getComponent(this.buttonTest2));
 		// since the button to bind this to is a property, this could be a loop of all buttons
 
 		// example of using events for inputs
 		this.controls.onPress('left', () => {
 
-			this.cameras.main.pan(500, (this.scale.height / 2), 8000, 'Sine.easeInOut');
 		});
+
+		this.spriteCount = 0;
 
 		this.controls.onPress('right', () => {
 
-			this.cameras.main.pan(2500, (this.scale.height / 2), 3000, 'Sine.easeInOut');
+			const pantsPrefab_0 = new PantsPrefab(this, Phaser.Math.RND.between(0, this.scale.width), Phaser.Math.RND.between(150, this.scale.height - 100));
+			this.add.existing(pantsPrefab_0);
+
+			// update sprite count
+			this.spriteCount++;
+			this.spriteCountText.setText('sprites: ' + this.spriteCount);
 		});
 
 		// CAMERA SETTINGS
@@ -246,8 +195,6 @@ class Level extends Phaser.Scene {
 
 		this.mobileText.setText('mobile: ' + this.registry.get('mobile'));
 
-		this.dustforce_SS_5.setScrollFactor(.5);
-		this.dustforce_SS.setScrollFactor(.5);
 
 		this.resize();
 	}
@@ -256,11 +203,9 @@ class Level extends Phaser.Scene {
 
 		if (this.controls.isDown('left')) {
 
-			this.pants.x -= 10;
 		}
 		else if (this.controls.isDown('right')) {
 
-			this.pants.x += 10;
 		}
 	}
 
---
+++ src/scenes/prefabs/PantsPrefab.js
@@ -0,0 +1,28 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class PantsPrefab extends Phaser.GameObjects.Sprite {
+
+	constructor(scene, x, y, texture, frame) {
+		super(scene, x ?? 0, y ?? 0, texture || "dino", frame);
+
+		// this (components)
+		new Animation(this);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
